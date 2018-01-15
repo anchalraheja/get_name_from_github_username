ead3e575891e4488ee4b98fe4f149fb353397692
Anchal Raheja <anchalpranav@gmail.com>
2018-01-15 13:03:56 -0600

    Update getusername.py

--- getusername.py
+++ getusername.py
@@ -13,7 +13,5 @@ for user in f.readlines():
 	userlink = ("http://www.github.com/"+ user)
 	website=urllib2.urlopen(userlink)
 	soup = BeautifulSoup(website, "lxml")
-	sys.stdout=open("test1.txt","a")
 	print soup.title.string
 	print userlink
-sys.stdout.close()
